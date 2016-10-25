e02e7b06f207ad720b5ec32aab7e32406f5087f9
Michael Leikind <mleikind@pinterest.com>
2016-10-25 15:29:27 -0700

    fixes

--- monitoring/hadoop_config_diff.py
+++ monitoring/hadoop_config_diff.py
@@ -14,12 +14,8 @@ HBASE_CONF_PATH = '/etc/hbase/conf'
 TIME = int(time.time())
 HOSTNAME = socket.gethostname()
 
-# variables that will never match local configs since they are mutated programaitcally in
-# the running configuration
 CONFIG_BLACKLIST = ['slave.host.name', 'dfs.datanode.hostname', 'mapreduce.jobtracker.webinterface.trusted',
-                    'hbase.dfs.client.read.shortcircuit.buffer.size', 'dfs.client.read.shortcircuit.buffer.size',
-                    'webinterface.private.actions', 'fs.s3.awsSecretAccessKey', 'fs.s3.awsAccessKeyId',
-                    'fs.s3n.awsSecretAccessKey', 'fs.s3n.awsAccessKeyId']
+                    'fs.s3n.awsSecretAccessKey', 'fs.s3n.awsAccessKeyId']  # Add more ...
 
 DAEMONS = {
     'namenode': ['50070', 'namenode'],
@@ -29,66 +25,25 @@ DAEMONS = {
 }
 
 
-# blacklist of configuration variables by port
 PORT_BLACKLIST = {
-    '16010': ['fs.defaultFS', 'hfile.block.cache.size', 'hbase.rootdir', 'hbase.client.retries.number',
+    '60010': ['fs.defaultFS', 'hfile.block.cache.size', 'hbase.rootdir', 'hbase.client.retries.number',
               'dfs.client.socket-timeout', 'dfs.datanode.socket.write.timeout'],
-    '16030': ['mapred.job.tracker', 'fs.defaultFS', 'hbase.rootdir', 'hbase.client.retries.number', 'dfs.client.socket-timeout',
+    '60030': ['mapred.job.tracker', 'fs.defaultFS', 'hbase.rootdir', 'hbase.client.retries.number', 'dfs.client.socket-timeout',
               'dfs.datanode.socket.write.timeout', 'hbase.master.cleaner.interval', 'hbase.master.logcleaner.ttl',
               'dfs.datanode.balance.bandwidthPerSec'],
     '50070': ['fs.defaultFS'],
     '50075': ['fs.defaultFS', 'dfs.namenode.name.dir', 'dfs.namenode.checkpoint.dir']
 }
 
-# keys to sort to look for ordering issues
 KEYS_TO_SORT = frozenset(['dfs.datanode.data.dir', 'dfs.namenode.name.dir', 'dfs.namenode.checkpoint.dir'])
 
-# list of deprecated properties. Sorry it is so big.
-# Hadoop changes it's mind quite a bit it seems.
 DEPRECATED_PROPERTIES = {
     "create.empty.dir.if.nonexist": "mapreduce.jobcontrol.createdir.ifnotexist",
-    "dfs.access.time.precision": "dfs.namenode.accesstime.precision",
-    "dfs.backup.address": "dfs.namenode.backup.address",
-    "dfs.backup.http.address": "dfs.namenode.backup.http-address",
-    "dfs.balance.bandwidthPerSec": "dfs.datanode.balance.bandwidthPerSec",
-    "dfs.block.size": "dfs.blocksize",
-    "dfs.data.dir": "dfs.datanode.data.dir",
-    "dfs.datanode.max.xcievers": "dfs.datanode.max.transfer.threads",
-    "dfs.df.interval": "fs.df.interval",
-    "dfs.federation.nameservice.id": "dfs.nameservice.id",
-    "dfs.federation.nameservices": "dfs.nameservices",
-    "dfs.http.address": "dfs.namenode.http-address",
-    "dfs.https.address": "dfs.namenode.https-address",
-    "dfs.https.client.keystore.resource": "dfs.client.https.keystore.resource",
-    "dfs.https.need.client.auth": "dfs.client.https.need-auth",
-    "dfs.max.objects": "dfs.namenode.max.objects",
-    "dfs.max-repl-streams": "dfs.namenode.replication.max-streams",
-    "dfs.name.dir": "dfs.namenode.name.dir",
-    "dfs.name.dir.restore": "dfs.namenode.name.dir.restore",
-    "dfs.name.edits.dir": "dfs.namenode.edits.dir",
-    "dfs.permissions": "dfs.permissions.enabled",
-    "dfs.permissions.supergroup": "dfs.permissions.superusergroup",
-    "dfs.read.prefetch.size": "dfs.client.read.prefetch.size",
-    "dfs.replication.considerLoad": "dfs.namenode.replication.considerLoad",
-    "dfs.replication.interval": "dfs.namenode.replication.interval",
-    "dfs.replication.min": "dfs.namenode.replication.min",
-    "dfs.replication.pending.timeout.sec": "dfs.namenode.replication.pending.timeout-sec",
-    "dfs.safemode.extension": "dfs.namenode.safemode.extension",
-    "dfs.safemode.threshold.pct": "dfs.namenode.safemode.threshold-pct",
-    "dfs.secondary.http.address": "dfs.namenode.secondary.http-address",
-    "dfs.socket.timeout": "dfs.client.socket-timeout",
-    "dfs.umaskmode": "fs.permissions.umask-mode",
-    "dfs.write.packet.size": "dfs.client-write-packet-size",
-    "fs.checkpoint.dir": "dfs.namenode.checkpoint.dir",
-    "fs.checkpoint.edits.dir": "dfs.namenode.checkpoint.edits.dir",
-    "fs.checkpoint.period": "dfs.namenode.checkpoint.period",
-    "fs.default.name": "fs.defaultFS",
-    "tasktracker.contention.tracking": "mapreduce.tasktracker.contention.tracking",
-    "topology.node.switch.mapping.impl": "net.topology.node.switch.mapping.impl",
     "topology.script.file.name": "net.topology.script.file.name",
     "topology.script.number.args": "net.topology.script.number.args",
     "user.name": "mapreduce.job.user.name",
     "webinterface.private.actions": "mapreduce.jobtracker.webinterface.trusted"
+    # Add more ...
 }
 
 
@@ -121,7 +76,6 @@ def _xml_to_dict(xml_string, port):
         except Exception:
             print('BlacklistNotFoundException: port {}'.format(port))
 
-        # TODO: Remove me after we fix yellow dashboards. Split by CSV, and sort these keys before adding to dict
         if property_name in KEYS_TO_SORT:
             property_value = sort_csv(children['value'].text.strip())
         else:
@@ -157,7 +111,6 @@ def get_configuration_from_url(port):
         except Exception:
             print('BlacklistNotFoundException: port {}'.format(port))
 
-        # TODO: Remove me after we fix yellow dashboards. Split by CSV, and sort these keys before adding to dict
         if _['key'] in KEYS_TO_SORT:
             _['value'] = sort_csv(_['value'])
 
@@ -280,4 +233,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
